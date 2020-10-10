b58db9dc6acb518c800912b9259ad4db72d5855a
Gustav Louw <airglouw@gmail.com>
2020-10-09 19:08:03 -0700

    quicker tests

--- tests/const.h
+++ tests/const.h
@@ -2,6 +2,6 @@
 
 #define MIN_STR_SIZE     (30) // SHORT STRING OPTIMIZATIONS NOT SUPPORTED.
 #define MAX_VALUE   (INT_MAX)
-#define MAX_SIZE       (8196)
-#define MAX_ITERS      (1000)
+#define MAX_SIZE       (1024)
+#define MAX_ITERS       (512)
 #define MAX_LETTERS      (23)
