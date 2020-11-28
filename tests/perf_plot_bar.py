4d51e14a7829484c218c740369c18523cac44c6d
Gustav Louw <airglouw@gmail.com>
2020-11-27 18:13:24 -0800

    update

--- tests/perf_plot_bar.py
+++ tests/perf_plot_bar.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     a = sys.argv[2]
     b = sys.argv[3]
-    compiler=['gcc -O3 -march=native perf_compile_c99.c', 'g++ -O3 -march=native perf_compile_cc.cc']
+    compiler=['tests/perf_compile_c99.c', 'tests/perf_compile_cc.cc']
     y = [float(a), float(b)]
     colors = ['crimson', 'darkslateblue']
     fig = go.Figure([go.Bar(x=compiler, y=y, text=y, textposition='auto', marker_color=colors)])
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     plot_bgcolor = "#F9F9F9"
     paper_bgcolor = "#F6F8FA"
     fig.update_layout(
-            title_text="CTL vs STL Compile Times, T = int, All Containers",
+            title_text="CTL vs STL Compile Times, T = int, All Containers (-O3 -march=native)",
             plot_bgcolor=plot_bgcolor, paper_bgcolor=paper_bgcolor)
     fig.write_image("%s.png" % filename, width=1000, height=300)
 
